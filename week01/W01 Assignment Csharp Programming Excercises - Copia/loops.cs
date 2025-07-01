9e319ea77db322436fc2ca2fc82423f1e559cda2
Klisman Gomes <76626836+klisman02@users.noreply.github.com>
2025-06-30 21:07:59 -0300

    Add files via upload

---
+++ week01/W01 Assignment Csharp Programming Excercises - Copia/loops.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        Console.WriteLine("Welcome to the Magic Number Game!");
+        Console.WriteLine("I have selected a magic number between 1 and 100.");
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
+    }
+}
